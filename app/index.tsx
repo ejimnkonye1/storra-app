4d2efc5ead413619a75d5f722d8dd97bdc57acab
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-09-24 00:30:26 +0100

    started this project

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function index(){
+    return(
+        <View >
+
+<Text className="text-red-400 text-center text-lg">Hello world</Text>
+</View>
+    )
+}
