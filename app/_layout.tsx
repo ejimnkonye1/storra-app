c19c2f82756348c4ab74fa1680ca0bd368b3792d
JUSTTNZ <chukwudiconfidenceosinachil@gmail.com>
2025-11-03 10:13:10 +0100

    refactoring cus of navigation bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,24 @@
-import { Stack } from "expo-router";
-import "./global.css";
-import { NavigationProvider } from "./context/NavigationContext";
-
+// app/_layout.tsx
+import { Stack } from 'expo-router'
+import './global.css'
+import { NavigationProvider } from './context/NavigationContext'
 
 export default function RootLayout() {
-
-
   return (
     <NavigationProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        {/* Onboarding or login routes (no tabs) */}
+        <Stack.Screen name="index"/>
+        <Stack.Screen name="login"/>
+        <Stack.Screen name="onboarding"/>
+
+        {/* Main app routes with bottom tabs */}
+        <Stack.Screen name="(tabs)"/>
       </Stack>
     </NavigationProvider>
-    
-  );
+  )
 }
