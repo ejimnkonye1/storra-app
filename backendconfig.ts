bc82372b1f54329c19eb4fa057e490573c8ba702
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-11-22 21:39:39 -0800

    done

--- backendconfig.ts
+++ backendconfig.ts
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'https://b42ab65c806b.ngrok-free.app/api/v1';
-export const BASE_URL = 'http://192.168.0.187:7001/api/v1';
+export const BASE_URL = 'https://storra-backend.vercel.app/api/v1';
